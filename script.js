fce447aeedcb64dad2d63522b2c83dd1e420f360
paradise-companyy <consparadisesl@gmail.com>
2026-07-15 19:02:24 +0200

    Add button click effect to scale on click

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ window.addEventListener("scroll", function () {
         nav.style.background = "rgba(0,0,0,.75)";
     }
 });
+
+
+const botones = document.querySelectorAll(".btn");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", function(){
+        this.style.transform="scale(0.95)";
+        setTimeout(()=>{
+            this.style.transform="scale(1)";
+        },150);
+    });
+});
